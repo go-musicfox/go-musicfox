a37fe73e4ee82e3614b721fd008e3c87f552589f
anhoder <anhoder@88.com>
2021-07-18 22:40:42 +0800

    update.

--- ui/player.go
+++ ui/player.go
@@ -356,7 +356,7 @@ func (p *Player) PlaySong(song ds.Song, duration PlayDirection) error {
     for _, artist := range song.Artists {
         artistNames = append(artistNames, artist.Name)
     }
-    utils.Notify(fmt.Sprintf("正在播放: %s", song.Name), fmt.Sprintf("歌手: %s 专辑: %s", strings.Join(artistNames, ","), song.Album.Name))
+    utils.Notify(fmt.Sprintf("正在播放: %s", song.Name), fmt.Sprintf("歌手: %s/专辑: %s", strings.Join(artistNames, ","), song.Album.Name))
 
     p.playErrCount = 0
 
