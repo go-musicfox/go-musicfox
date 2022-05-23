9ea08db090aba0050badf63ddce1c6e69cb445f5
anhoder <anhoder@88.com>
2022-05-23 21:50:57 +0800

    feat. 更新版本

--- constants/constants.go
+++ constants/constants.go
@@ -5,8 +5,8 @@ import (
 )
 
 const AppName = "musicfox"
-const AppVersion = "2.1.0"
-const AppVersionInt = 20100
+const AppVersion = "2.2.0"
+const AppVersionInt = 20200
 const AppDescription = "<cyan>Musicfox - 命令行版网易云音乐</>"
 const AppShowStartup = true
 const AppGithubUrl = "https://github.com/anhoder/go-musicfox"
