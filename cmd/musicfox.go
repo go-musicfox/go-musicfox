82de75ea3ea67daa5e67a3e277f53a535ad7848f
anhoder <anhoder@88.com>
2022-09-06 23:10:40 +0800

    stash

--- cmd/musicfox.go
+++ cmd/musicfox.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "github.com/progrium/macdriver/cocoa"
 
     "github.com/gookit/gcli/v2"
     "go-musicfox/pkg/commands"
@@ -11,24 +12,32 @@ import (
 )
 
 func main() {
-    app := gcli.NewApp()
-    app.Name = constants.AppName
-    app.Version = constants.AppVersion
-    app.Description = constants.AppDescription
+    go func() {
+        app := gcli.NewApp()
+        app.Name = constants.AppName
+        app.Version = constants.AppVersion
+        app.Description = constants.AppDescription
 
-    // 加载config
-    utils.LoadIniConfig()
+        // 加载config
+        utils.LoadIniConfig()
 
-    logo := utils.GetAlphaAscii(app.Name)
-    randomColor := ui.GetPrimaryColor()
-    logoColorful := ui.SetFgStyle(logo, randomColor)
+        logo := utils.GetAlphaAscii(app.Name)
+        randomColor := ui.GetPrimaryColor()
+        logoColorful := ui.SetFgStyle(logo, randomColor)
 
-    gcli.AppHelpTemplate = fmt.Sprintf(constants.AppHelpTemplate, logoColorful)
-    app.Logo.Text = logoColorful
+        gcli.AppHelpTemplate = fmt.Sprintf(constants.AppHelpTemplate, logoColorful)
+        app.Logo.Text = logoColorful
 
-    playerCommand := commands.NewPlayerCommand()
-    app.Add(playerCommand)
-    app.DefaultCommand(playerCommand.Name)
+        playerCommand := commands.NewPlayerCommand()
+        app.Add(playerCommand)
+        app.DefaultCommand(playerCommand.Name)
 
+        app.Run()
+    }()
+
+    app := cocoa.NSApp()
+    app.SetActivationPolicy(cocoa.NSApplicationActivationPolicyProhibited)
+    app.ActivateIgnoringOtherApps(true)
     app.Run()
+
 }
