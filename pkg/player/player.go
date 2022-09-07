78ba98c6972a4959ec47fa104d341807f87b6af0
anhoder <anhoder@88.com>
2022-09-07 09:50:02 +0800

    update

--- pkg/player/player.go
+++ pkg/player/player.go
@@ -1,338 +1,339 @@
 package player
 
 import (
-    "fmt"
-    "go-musicfox/utils"
-    "io"
-    "net/http"
-    "os"
-    "time"
+	"fmt"
+	"go-musicfox/utils"
+	"io"
+	"net/http"
+	"os"
+	"time"
 
-    "github.com/faiface/beep"
-    "github.com/faiface/beep/effects"
-    "github.com/faiface/beep/flac"
-    "github.com/faiface/beep/mp3"
-    "github.com/faiface/beep/speaker"
-    "github.com/faiface/beep/vorbis"
-    "github.com/faiface/beep/wav"
-    "github.com/progrium/macdriver/mediaplayer"
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/effects"
+	"github.com/faiface/beep/flac"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/vorbis"
+	"github.com/faiface/beep/wav"
+	"github.com/progrium/macdriver/mediaplayer"
 )
 
 // State 播放器状态
 type State uint8
 
 const (
-    Stopped State = iota
-    Paused
-    Playing
+	Stopped State = iota
+	Paused
+	Playing
 )
 
 // SongType 歌曲类型
 type SongType uint8
 
 const (
-    Mp3 SongType = iota
-    Wav
-    Ogg
-    Flac
+	Mp3 SongType = iota
+	Wav
+	Ogg
+	Flac
 )
 
 var (
-    nowPlayingCenter *mediaplayer.MPNowPlayingInfoCenter
-    remoteCommandCenter *mediaplayer.MPRemoteCommandCenter
+	nowPlayingCenter    *mediaplayer.MPNowPlayingInfoCenter
+	remoteCommandCenter *mediaplayer.MPRemoteCommandCenter
 )
 
 type UrlMusic struct {
-    Url      string
-    Type     SongType
-    Duration time.Duration
+	Url      string
+	Type     SongType
+	Duration time.Duration
 }
 
 type Player struct {
-    State    State
-    Progress int
-    CurMusic UrlMusic
-    ctrl     *beep.Ctrl
-    volume   *effects.Volume
-    Timer    *utils.Timer
-    timeChan chan time.Duration
-    done     chan struct{}
+	State    State
+	Progress int
+	CurMusic UrlMusic
+	ctrl     *beep.Ctrl
+	volume   *effects.Volume
+	Timer    *utils.Timer
+	timeChan chan time.Duration
+	done     chan struct{}
 
-    musicChan chan UrlMusic
+	musicChan chan UrlMusic
 }
 
 func NewPlayer() *Player {
-    player := new(Player)
-    player.timeChan = make(chan time.Duration)
-    player.done = make(chan struct{})
-    player.musicChan = make(chan UrlMusic)
-    player.ctrl = &beep.Ctrl{
-        Paused: false,
-    }
-    player.volume = &effects.Volume{
-        Base:   2,
-        Silent: false,
-    }
+	player := new(Player)
+	player.timeChan = make(chan time.Duration)
+	player.done = make(chan struct{})
+	player.musicChan = make(chan UrlMusic)
+	player.ctrl = &beep.Ctrl{
+		Paused: false,
+	}
+	player.volume = &effects.Volume{
+		Base:   2,
+		Silent: false,
+	}
 
-    go func() {
-        player.listen()
-    }()
+	go func() {
+		player.listen()
+	}()
 
-    if nowPlayingCenter != nil && remoteCommandCenter != nil {
-        registerCommands(player)
-    }
+	if nowPlayingCenter != nil && remoteCommandCenter != nil {
+		registerCommands(player)
+	}
 
-    return player
+	return player
 }
 
 // listen 开始监听
 func (p *Player) listen() {
-    var sampleRate beep.SampleRate = 44100
-    err := speaker.Init(sampleRate, sampleRate.N(time.Millisecond*200))
-    if err != nil {
-        panic(err)
-    }
-    done := make(chan bool)
+	var sampleRate beep.SampleRate = 44100
+	err := speaker.Init(sampleRate, sampleRate.N(time.Millisecond*200))
+	if err != nil {
+		panic(err)
+	}
+	done := make(chan bool)
 
-    var (
-        streamer, oldStreamer  beep.StreamSeekCloser
-        cacheRFile, cacheWFile *os.File
-    )
+	var (
+		streamer, oldStreamer  beep.StreamSeekCloser
+		cacheRFile, cacheWFile *os.File
+	)
 
-    cacheFile := fmt.Sprintf("%s/music_cache", utils.GetLocalDataDir())
+	cacheFile := fmt.Sprintf("%s/music_cache", utils.GetLocalDataDir())
 
-    for {
-        select {
-        case <-done:
-            if nowPlayingCenter != nil {
-                nowPlayingCenter.SetPlaybackState_(mediaplayer.MPNowPlayingPlaybackStateStopped)
-                nowPlayingCenter.SetNowPlayingInfo_(nowPlayingInfo(p))
-            }
-            p.State = Stopped
-            p.pushDone()
-            break
-        case p.CurMusic = <-p.musicChan:
-            var (
-                resp   *http.Response
-                format beep.Format
-            )
+	for {
+		select {
+		case <-done:
+			if nowPlayingCenter != nil {
+				nowPlayingCenter.SetPlaybackState_(mediaplayer.MPNowPlayingPlaybackStateStopped)
+				nowPlayingCenter.SetNowPlayingInfo_(nowPlayingInfo(p))
+			}
+			p.State = Stopped
+			p.pushDone()
+			break
+		case p.CurMusic = <-p.musicChan:
+			var (
+				resp   *http.Response
+				format beep.Format
+			)
 
-            // FIXME 先这样处理，暂时没想到更好的办法
-            // 打开缓存文件
-            if cacheRFile != nil {
-                _ = cacheRFile.Close()
-            }
-            if cacheWFile != nil {
-                _ = cacheWFile.Close()
-            }
-            cacheRFile, err = os.OpenFile(cacheFile, os.O_CREATE|os.O_TRUNC|os.O_RDONLY, 0666)
-            if err != nil {
-                panic(err)
-            }
-            cacheWFile, err = os.OpenFile(cacheFile, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
-            if err != nil {
-                panic(err)
-            }
+			// FIXME 先这样处理，暂时没想到更好的办法
+			// 打开缓存文件
+			if cacheRFile != nil {
+				_ = cacheRFile.Close()
+			}
+			if cacheWFile != nil {
+				_ = cacheWFile.Close()
+			}
+			cacheRFile, err = os.OpenFile(cacheFile, os.O_CREATE|os.O_TRUNC|os.O_RDONLY, 0666)
+			if err != nil {
+				panic(err)
+			}
+			cacheWFile, err = os.OpenFile(cacheFile, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+			if err != nil {
+				panic(err)
+			}
 
-            speaker.Clear()
+			speaker.Clear()
 
-            // 关闭旧响应body
-            if resp != nil {
-                _ = resp.Body.Close()
-            }
+			// 关闭旧响应body
+			if resp != nil {
+				_ = resp.Body.Close()
+			}
 
-            // 关闭旧计时器
-            if p.Timer != nil {
-                p.Timer.Stop()
-            }
-            p.Progress = 0
+			// 关闭旧计时器
+			if p.Timer != nil {
+				p.Timer.Stop()
+			}
+			p.Progress = 0
 
-            resp, err = http.Get(p.CurMusic.Url)
-            if err != nil {
-                p.pushDone()
-                break
-            }
+			resp, err = http.Get(p.CurMusic.Url)
+			if err != nil {
+				p.pushDone()
+				break
+			}
 
-            go func(cacheWFile *os.File, read io.ReadCloser) {
-                _, _ = io.Copy(cacheWFile, read)
-            }(cacheWFile, resp.Body)
+			go func(cacheWFile *os.File, read io.ReadCloser) {
+				_, _ = io.Copy(cacheWFile, read)
+			}(cacheWFile, resp.Body)
 
-            for {
-                t := make([]byte, 5)
-                _, err = io.ReadFull(cacheRFile, t)
-                if err != io.EOF {
-                    _, _ = cacheRFile.Seek(0, 0)
-                    break
-                }
-            }
+			for {
+				t := make([]byte, 5)
+				_, err = io.ReadFull(cacheRFile, t)
+				if err != io.EOF {
+					_, _ = cacheRFile.Seek(0, 0)
+					break
+				}
+			}
 
-            oldStreamer = streamer
-            switch p.CurMusic.Type {
-            case Mp3:
-                streamer, format, err = mp3.Decode(cacheRFile)
-            case Wav:
-                streamer, format, err = wav.Decode(cacheRFile)
-            case Ogg:
-                streamer, format, err = vorbis.Decode(cacheRFile)
-            case Flac:
-                streamer, format, err = flac.Decode(cacheRFile)
-            default:
-                p.pushDone()
-                break
-            }
-            if err != nil {
-                p.pushDone()
-                break
-            }
+			oldStreamer = streamer
+			switch p.CurMusic.Type {
+			case Mp3:
+				streamer, format, err = mp3.Decode(cacheRFile)
+			case Wav:
+				streamer, format, err = wav.Decode(cacheRFile)
+			case Ogg:
+				streamer, format, err = vorbis.Decode(cacheRFile)
+			case Flac:
+				streamer, format, err = flac.Decode(cacheRFile)
+			default:
+				p.pushDone()
+				break
+			}
+			if err != nil {
+				p.pushDone()
+				break
+			}
 
-            p.State = Playing
-            newStreamer := beep.Resample(3, format.SampleRate, sampleRate, streamer)
-            p.ctrl.Streamer = beep.Seq(newStreamer, beep.Callback(func() {
-                done <- true
-            }))
-            p.volume.Streamer = p.ctrl
-            p.ctrl.Paused = false
-            speaker.Play(p.volume)
+			p.State = Playing
+			newStreamer := beep.Resample(3, format.SampleRate, sampleRate, streamer)
+			p.ctrl.Streamer = beep.Seq(newStreamer, beep.Callback(func() {
+				done <- true
+			}))
+			p.volume.Streamer = p.ctrl
+			p.ctrl.Paused = false
+			speaker.Play(p.volume)
 
-            // 启动计时器
-            p.Timer = utils.New(utils.Options{
-                Duration:       24 * time.Hour,
-                TickerInternal: 200 * time.Millisecond,
-                OnRun:          func(started bool) {},
-                OnPaused:       func() {},
-                OnDone:         func(stopped bool) {},
-                OnTick: func() {
-                    if p.CurMusic.Duration > 0 {
-                        p.Progress = int(p.Timer.Passed() * 100 / p.CurMusic.Duration)
-                    }
-                    select {
-                    case p.timeChan <- p.Timer.Passed():
-                    default:
-                    }
-                },
-            })
-            go p.Timer.Run()
+			// 启动计时器
+			p.Timer = utils.New(utils.Options{
+				Duration:       24 * time.Hour,
+				TickerInternal: 200 * time.Millisecond,
+				OnRun:          func(started bool) {},
+				OnPaused:       func() {},
+				OnDone:         func(stopped bool) {},
+				OnTick: func() {
+					if p.CurMusic.Duration > 0 {
+						p.Progress = int(p.Timer.Passed() * 100 / p.CurMusic.Duration)
+					}
+					select {
+					case p.timeChan <- p.Timer.Passed():
+					default:
+					}
+				},
+			})
 
-            if nowPlayingCenter != nil {
-                nowPlayingCenter.SetPlaybackState_(mediaplayer.MPNowPlayingPlaybackStatePlaying)
-                nowPlayingCenter.SetNowPlayingInfo_(nowPlayingInfo(p))
-            }
+			if nowPlayingCenter != nil {
+				nowPlayingCenter.SetPlaybackState_(mediaplayer.MPNowPlayingPlaybackStatePlaying)
+				nowPlayingCenter.SetNowPlayingInfo_(nowPlayingInfo(p))
+			}
 
-            // 关闭旧Streamer，避免协程泄漏
-            if oldStreamer != nil {
-                _ = oldStreamer.Close()
-            }
-        }
+			go p.Timer.Run()
 
-    }
+			// 关闭旧Streamer，避免协程泄漏
+			if oldStreamer != nil {
+				_ = oldStreamer.Close()
+			}
+		}
+
+	}
 
 }
 
 // Play 播放音乐
 func (p *Player) Play(songType SongType, url string, duration time.Duration) {
-    if nowPlayingCenter != nil {
-        nowPlayingCenter.SetPlaybackState_(mediaplayer.MPNowPlayingPlaybackStatePaused)
-    }
+	if nowPlayingCenter != nil {
+		nowPlayingCenter.SetPlaybackState_(mediaplayer.MPNowPlayingPlaybackStateStopped)
+	}
 
-    music := UrlMusic{
-        url,
-        songType,
-        duration,
-    }
-    select {
-    case p.musicChan <- music:
-    default:
-    }
+	music := UrlMusic{
+		url,
+		songType,
+		duration,
+	}
+	select {
+	case p.musicChan <- music:
+	default:
+	}
 }
 
 func (p *Player) pushDone() {
-    select {
-    case p.done <- struct{}{}:
-    default:
-    }
+	select {
+	case p.done <- struct{}{}:
+	default:
+	}
 }
 
 // TimeChan 获取定时器
 func (p *Player) TimeChan() <-chan time.Duration {
-    return p.timeChan
+	return p.timeChan
 }
 
 // Done done chan, 如果播放完成往chan中写入struct{}
 func (p *Player) Done() <-chan struct{} {
-    return p.done
+	return p.done
 }
 
 // UpVolume 调大音量
 func (p *Player) UpVolume() {
-    if p.volume.Volume > 0 {
-        return
-    }
+	if p.volume.Volume > 0 {
+		return
+	}
 
-    speaker.Lock()
-    p.volume.Silent = false
-    p.volume.Volume += 0.5
-    speaker.Unlock()
+	speaker.Lock()
+	p.volume.Silent = false
+	p.volume.Volume += 0.5
+	speaker.Unlock()
 }
 
 // DownVolume 调小音量
 func (p *Player) DownVolume() {
-    if p.volume.Volume <= -5 {
-        speaker.Lock()
-        p.volume.Silent = true
-        speaker.Unlock()
-        return
-    }
+	if p.volume.Volume <= -5 {
+		speaker.Lock()
+		p.volume.Silent = true
+		speaker.Unlock()
+		return
+	}
 
-    speaker.Lock()
-    p.volume.Volume -= 0.5
-    speaker.Unlock()
+	speaker.Lock()
+	p.volume.Volume -= 0.5
+	speaker.Unlock()
 }
 
 // Paused 暂停播放
 func (p *Player) Paused() {
-    if p.State == Paused {
-        return
-    }
+	if p.State == Paused {
+		return
+	}
 
-    if nowPlayingCenter != nil {
-        nowPlayingCenter.SetPlaybackState_(mediaplayer.MPNowPlayingPlaybackStatePaused)
-        nowPlayingCenter.SetNowPlayingInfo_(nowPlayingInfo(p))
-    }
+	if nowPlayingCenter != nil {
+		nowPlayingCenter.SetPlaybackState_(mediaplayer.MPNowPlayingPlaybackStatePaused)
+		nowPlayingCenter.SetNowPlayingInfo_(nowPlayingInfo(p))
+	}
 
-    speaker.Lock()
-    p.ctrl.Paused = true
-    speaker.Unlock()
-    p.State = Paused
-    p.Timer.Pause()
+	speaker.Lock()
+	p.ctrl.Paused = true
+	speaker.Unlock()
+	p.State = Paused
+	p.Timer.Pause()
 }
 
 // Resume 继续播放
 func (p *Player) Resume() {
-    if p.State == Playing {
-        return
-    }
+	if p.State == Playing {
+		return
+	}
 
-    speaker.Lock()
-    p.ctrl.Paused = false
-    speaker.Unlock()
-    p.State = Playing
-    go p.Timer.Run()
+	speaker.Lock()
+	p.ctrl.Paused = false
+	speaker.Unlock()
+	p.State = Playing
+	go p.Timer.Run()
 
-    if nowPlayingCenter != nil {
-        nowPlayingCenter.SetNowPlayingInfo_(nowPlayingInfo(p))
-        nowPlayingCenter.SetPlaybackState_(mediaplayer.MPNowPlayingPlaybackStatePlaying)
-    }
+	if nowPlayingCenter != nil {
+		nowPlayingCenter.SetNowPlayingInfo_(nowPlayingInfo(p))
+		nowPlayingCenter.SetPlaybackState_(mediaplayer.MPNowPlayingPlaybackStatePlaying)
+	}
 }
 
 // Close 关闭
 func (p *Player) Close() {
-    if nowPlayingCenter != nil {
-        nowPlayingCenter.Release()
-    }
-    if remoteCommandCenter != nil {
-        remoteCommandCenter.Release()
-    }
+	if nowPlayingCenter != nil {
+		nowPlayingCenter.Release()
+	}
+	if remoteCommandCenter != nil {
+		remoteCommandCenter.Release()
+	}
 
-    p.Timer.Stop()
-    speaker.Clear()
+	p.Timer.Stop()
+	speaker.Clear()
 }
