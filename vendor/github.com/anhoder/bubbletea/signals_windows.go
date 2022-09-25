726a076e297ee848c8377d0c875c172cf63c5909
anhoder <anhoder@88.com>
2022-09-25 17:11:25 +0800

    feat: 优化windows resize

--- vendor/github.com/anhoder/bubbletea/signals_windows.go
+++ vendor/github.com/anhoder/bubbletea/signals_windows.go
@@ -13,7 +13,7 @@ import (
 // listenForResize is not available on windows because windows does not
 // implement syscall.SIGWINCH.
 func listenForResize(output *os.File, msgs chan Msg, errs chan error) {
-	ticker := time.NewTimer(time.Second)
+	ticker := time.NewTicker(time.Second)
 	var width, height int
 	for range ticker.C {
 		w, h, err := terminal.GetSize(int(output.Fd()))
