ba603fdd8f7155ea2f5fbfaebb570cd20c3d04f9
anhoder <anhoder@88.com>
2023-03-24 22:58:51 +0800

    feat: 更新github action

---
+++ hack/scoop_gen.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o pipefail
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+if [[ -z "$GITHUB_REF" ]]; then
+  echo "GITHUB_REF is empty"
+  exit -1
+fi
+
+SCOOP_VERSION=${GITHUB_REF#refs/tags/}
+SCOOP_VERSION=${SCOOP_VERSION#v}
+
+eval "cat <<EOF
+$(< "$ROOT"/deploy/scoop/go-musicfox.json.tpl)
+EOF
+"  > "$ROOT"/deploy/scoop/go-musicfox.json
