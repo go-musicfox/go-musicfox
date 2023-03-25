1723222853b57d4cfa3c974ccbb966e19d31eec7
anhoder <anhoder@88.com>
2023-03-25 11:53:57 +0800

    fix: github action

--- hack/scoop_gen.sh
+++ hack/scoop_gen.sh
@@ -15,6 +15,8 @@ fi
 SCOOP_VERSION=${GITHUB_REF#refs/tags/}
 SCOOP_VERSION=${SCOOP_VERSION#v}
 
+SCOOP_HASH="$(curl -s https://github.com/go-musicfox/go-musicfox/releases/download/v${SCOOP_VERSION}/go-musicfox_${SCOOP_VERSION}_windows_amd64.zip | sha256sum)"
+
 eval "cat <<EOF
 $(< "$ROOT"/deploy/scoop/go-musicfox.json.tpl)
 EOF
