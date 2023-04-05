e03e3deb2ffa8f6ab4e74d29a3afcaf3ba47b1fa
anhoder <anhoder@88.com>
2023-04-05 21:38:13 +0800

    feat: purego替换

---
+++ pkg/macdriver/avcore/avcore.go
@@ -0,0 +1,27 @@
+//go:build darwin
+
+package avcore
+
+import (
+	"sync"
+
+	"github.com/ebitengine/purego"
+)
+
+var importOnce sync.Once
+
+func importFramework() {
+	importOnce.Do(func() {
+		_, err := purego.Dlopen("AVFoundation.framework/AVFoundation", purego.RTLD_GLOBAL)
+		if err != nil {
+			panic(err)
+		}
+	})
+}
+
+type CMTime struct {
+	Value     int64
+	Timescale int32
+	Flags     uint32
+	Epoch     int64
+}
