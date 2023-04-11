90352f20e3aa9f0b7e7582ed794b77759e12b74c
anhoder <anhoder@88.com>
2023-04-11 23:48:31 +0800

    feat: 代码优化

--- utils/recover.go
+++ utils/recover.go
@@ -1,14 +1,18 @@
 package utils
 
-import "runtime/debug"
+import (
+	"runtime/debug"
+)
 
-func Recover(ignore bool) {
+func Recover(ignore bool) (hasCaught bool) {
 	err := recover()
 	if err != nil {
 		Logger().Printf("catch panic, err: %+v, stack: %s", err, debug.Stack())
 		if ignore {
+			hasCaught = true
 			return
 		}
 		panic(err)
 	}
+	return
 }
