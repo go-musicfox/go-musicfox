77f8dd84fd02a9f815b3e717e31fe04843d78941
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-06-09 13:15:57 +0800

    Add flake support

--- flake.nix
+++ flake.nix
@@ -23,16 +23,16 @@
         mission-control.scripts = {
           run = {
             description = "Run app";
-            exec = "go run ../../cmd/musicfox.go";
+            exec = "go run ./cmd/musicfox.go";
             category = "Dev Tools";
           };
         };
 
-        packages.default = pkgs.callPackage ./default.nix { };
+        packages.default = pkgs.callPackage ./deploy/nix { };
         overlayAttrs = {
           inherit (config.packages) go-musicfox;
         };
-        packages.go-musicfox = pkgs.callPackage ./default.nix { };
+        packages.go-musicfox = pkgs.callPackage ./deploy/nix { };
 
         devShells.default = pkgs.mkShell {
           inputsFrom = [
