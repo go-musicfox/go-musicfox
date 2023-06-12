8dfe8a2e2d82951b5cf085db6f11c9a8ff3b9a66
anhoder <anhoder@88.com>
2023-06-12 09:19:22 +0800

    feat: 配置命令优化

--- pkg/commands/config.go
+++ pkg/commands/config.go
@@ -2,11 +2,13 @@ package commands
 
 import (
 	"fmt"
+	"path"
 
 	"github.com/go-musicfox/go-musicfox/pkg/constants"
+	"github.com/go-musicfox/go-musicfox/pkg/ui"
 	"github.com/go-musicfox/go-musicfox/utils"
-
 	"github.com/gookit/gcli/v2"
+	"github.com/muesli/termenv"
 )
 
 func NewConfigCommand() *gcli.Command {
@@ -14,7 +16,8 @@ func NewConfigCommand() *gcli.Command {
 		Name:   "config",
 		UseFor: "Print configuration file to be loaded",
 		Func: func(_ *gcli.Command, _ []string) error {
-			fmt.Printf("\nLoaded Configuration File: %s/%s\n", utils.GetLocalDataDir(), constants.AppIniFile)
+			var configPath = ui.SetFgStyle(path.Join(utils.GetLocalDataDir(), constants.AppIniFile), termenv.ANSICyan)
+			fmt.Printf("\nLoaded Configuration File: %s\n", configPath)
 			return nil
 		},
 	}
