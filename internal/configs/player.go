4997db79883695bd5dc5aebef8437c1d0a517e2e
ruisenlin <ruisenlin@xiaoman.cn>
2024-07-18 11:11:52 +0800

    feat: add maxPlayErrCount config

--- internal/configs/player.go
+++ internal/configs/player.go
@@ -1,11 +1,12 @@
 package configs
 
 type PlayerOptions struct {
-	Engine         string // 播放引擎
-	BeepMp3Decoder string // beep mp3解码器
-	MpdBin         string // mpd路径
-	MpdConfigFile  string // mpd配置文件
-	MpdNetwork     string // mpd网络类型: tcp、unix
-	MpdAddr        string // mpd地址
-	MpdAutoStart   bool   // mpd自动启动
+	Engine          string // 播放引擎
+	BeepMp3Decoder  string // beep mp3解码器
+	MpdBin          string // mpd路径
+	MpdConfigFile   string // mpd配置文件
+	MpdNetwork      string // mpd网络类型: tcp、unix
+	MpdAddr         string // mpd地址
+	MpdAutoStart    bool   // mpd自动启动
+	MaxPlayErrCount int    // mpd自动启动
 }
