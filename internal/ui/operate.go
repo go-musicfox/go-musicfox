d34f44ca9b22fa388f8d4277093b3de56b06196f
imxyy_soope_ <3516554684@qq.com>
2024-07-26 12:04:18 +0800

    fix build

--- internal/ui/operate.go
+++ internal/ui/operate.go
@@ -808,10 +808,10 @@ func DownLoadLrc(m *Netease) {
 	loading.Start()
 	defer loading.Complete()
 
-	if m.player.curSongIndex >= len(m.player.playlist) {
+	if m.player.CurSongIndex() >= len(m.player.Playlist()) {
 		return
 	}
-	curSong := m.player.playlist[m.player.curSongIndex]
+	curSong := m.player.CurSong()
 	//utils.DownloadLrc(curSong)
 	lrcService := service.LyricService{
 		ID: strconv.FormatInt(curSong.Id, 10),
