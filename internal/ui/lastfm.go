20f588a39c5fbc0a65da16832396ae4b71c8ae46
jbwfu <75001777+jbwfu@users.noreply.github.com>
2025-03-09 21:12:03 +0800

    feat(last.fm): support toggle scrobble status

--- internal/ui/lastfm.go
+++ internal/ui/lastfm.go
@@ -23,25 +23,37 @@ func (m *Lastfm) GetMenuKey() string {
 	return "last_fm"
 }
 
+var getScrobbleSubtitle = func(m *lastfm.Tracker) string {
+	if m.Status() {
+		return "[已启用]"
+	}
+	return "[未启用]"
+}
+
 func (m *Lastfm) MenuViews() []model.MenuItem {
 	if !lastfm.IsAvailable() {
 		return []model.MenuItem{
 			{Title: "当前不可用，请设置 API key 及 secret"},
+			{Title: "本地记录", Subtitle: getScrobbleSubtitle(m.netease.lastfm.Tracker)},
 		}
 	}
 
 	if m.netease.lastfm.NeedAuth() {
 		return []model.MenuItem{
 			{Title: "去授权"},
+			{Title: "本地记录", Subtitle: getScrobbleSubtitle(m.netease.lastfm.Tracker)},
 		}
 	}
+
 	return []model.MenuItem{
 		{Title: "查看用户信息"},
+		{Title: "切换功能状态", Subtitle: getScrobbleSubtitle(m.netease.lastfm.Tracker)},
 		{Title: "清除授权"},
 	}
+
 }
 
-func (m *Lastfm) SubMenu(_ *model.App, index int) model.Menu {
+func (m *Lastfm) SubMenu(app *model.App, index int) model.Menu {
 	switch index {
 	case 0:
 		if !lastfm.IsAvailable() {
@@ -52,6 +64,9 @@ func (m *Lastfm) SubMenu(_ *model.App, index int) model.Menu {
 		}
 		m.netease.lastfm.OpenUserHomePage()
 	case 1:
+		m.netease.lastfm.Tracker.Toggle()
+		app.MustMain().RefreshMenuList()
+	case 2:
 		m.netease.lastfm.ClearUserInfo()
 		return NewLastfmRes(m.baseMenu, "清除授权", nil, 2)
 	}
