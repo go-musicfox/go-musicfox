f64d58c2aaed627d01afa64e4ac88967ab03f2a5
jbwfu <75001777+jbwfu@users.noreply.github.com>
2025-09-01 15:39:42 +0800

    fix: action menu items

--- internal/ui/action_select.go
+++ internal/ui/action_select.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"github.com/anhoder/foxful-cli/model"
+	"github.com/go-musicfox/go-musicfox/internal/composer"
 )
 
 const actionMenuKey = "action_menu"
@@ -18,7 +19,7 @@ type ActionMenu struct {
 	baseMenu
 	from    string // 发起 action 的页面
 	playing bool   // 是否针对当前播放
-	item    []ActionItem
+	items    []ActionItem
 }
 
 // NewActionMenu 新建操作页
@@ -35,8 +36,8 @@ func (m *ActionMenu) GetMenuKey() string {
 }
 
 func (m *ActionMenu) MenuViews() []model.MenuItem {
-	menuItems := make([]model.MenuItem, 0, len(m.item))
-	for _, item := range m.item {
+	menuItems := make([]model.MenuItem, 0, len(m.items))
+	for _, item := range m.items {
 		menuItems = append(menuItems, item.title)
 	}
 	return menuItems
@@ -46,138 +47,138 @@ func (m *ActionMenu) SubMenu(app *model.App, index int) model.Menu {
 	// FIXME: 快速返回后执行操作，以达到直接调用的（Loading）显示效果。（异步？）
 	app.MustMain().BackMenu()
 	app.MustMain().RefreshMenuList()
-	if m.item[index].action != nil {
-		m.item[index].action()
+	if m.items[index].action != nil {
+		m.items[index].action()
 		return nil
 	}
-	if m.item[index].page != nil {
-		return NewMenuToPage(m.baseMenu, m.item[index].page())
+	if m.items[index].page != nil {
+		return NewMenuToPage(m.baseMenu, m.items[index].page())
 	}
 
 	return nil
 }
 
 func (m *ActionMenu) BeforeEnterMenuHook() model.Hook {
-	// slog.Info(fmt.Sprintf("from: %v, isplaying: %v", m.from, m.playing))
+	isSelected := !m.playing
+	var actions []ActionItem
+	main := m.netease.MustMain()
+	menu := main.CurMenu()
 
-	var (
-		main = m.netease.MustMain()
-		menu = main.CurMenu()
-	)
-
-	switch menu.(type) {
-	case SongsMenu:
-		if m.playing {
-			m.curSongAction()
-		} else {
-			m.selectSongAction()
-		}
-	case PlaylistsMenu:
-		m.playlistAction()
-	default:
-		m.curSongAction()
+	if m.playing || isSongsProvider(menu) {
+		actions = append(actions, buildSongActions(m.netease, isSelected)...)
 	}
-	return nil
-}
 
-func (m *ActionMenu) selectSongAction() {
-	m.item = []ActionItem{
-		{
-			title:  model.MenuItem{Title: "所属专辑"},
-			action: func() { goToAlbumOfSong(m.netease, true) },
-		}, {
-			title:  model.MenuItem{Title: "所属歌手"},
-			action: func() { goToArtistOfSong(m.netease, true) },
-		}, {
-			title:  model.MenuItem{Title: "下载"},
-			action: func() { downloadSong(m.netease, true) },
-		}, {
-			title:  model.MenuItem{Title: "下载歌词"},
-			action: func() { downloadSongLrc(m.netease, true) },
-		}, {
-			title: model.MenuItem{Title: "添加到喜欢"},
-			page:  func() model.Page { return likeSong(m.netease, true, true) },
-		}, {
-			title: model.MenuItem{Title: "从喜欢移除"},
-			page:  func() model.Page { return likeSong(m.netease, false, true) },
-		}, {
-			title: model.MenuItem{Title: "添加至歌单"},
-			page:  func() model.Page { return openAddSongToUserPlaylistMenu(m.netease, true, true) },
-		}, {
-			title: model.MenuItem{Title: "从歌单移除"},
-			page:  func() model.Page { return openAddSongToUserPlaylistMenu(m.netease, true, false) },
-		}, {
-			title:  model.MenuItem{Title: "在网页打开"},
-			action: func() { openInWeb(m.netease, true) },
-		}, {
-			title: model.MenuItem{Title: "标记为不喜欢"},
-			page:  func() model.Page { return trashSong(m.netease, true) },
-		}, {
-			title:  model.MenuItem{Title: "相似的歌曲"},
-			action: func() { findSimilarSongs(m.netease, true) },
-		}, {
-			title:  model.MenuItem{Title: "分享"},
-			action: func() { shareItem(m.netease, true) },
-		},
+	if canCollectPlaylist(menu) {
+		actions = append(actions, buildPlaylistActions(m.netease)...)
 	}
-	if m.from == CurPlaylistKey { // 仅在当前播放界面生效
-		m.item = append(m.item, ActionItem{
+
+	if isSelected && m.from == CurPlaylistKey {
+		actions = append(actions, ActionItem{
 			title: model.MenuItem{Title: "从播放列表移除"},
 			page:  func() model.Page { return delSongFromPlaylist(m.netease) },
 		})
 	}
-}
 
-func (m *ActionMenu) curSongAction() {
-	m.item = []ActionItem{
-		{
-			title:  model.MenuItem{Title: "所属专辑"},
-			action: func() { goToAlbumOfSong(m.netease, false) },
-		}, {
-			title:  model.MenuItem{Title: "所属歌手"},
-			action: func() { goToArtistOfSong(m.netease, false) },
-		}, {
-			title:  model.MenuItem{Title: "下载音乐"},
-			action: func() { downloadSong(m.netease, false) },
-		}, {
-			title:  model.MenuItem{Title: "下载歌词"},
-			action: func() { downloadSongLrc(m.netease, false) },
-		}, {
-			title: model.MenuItem{Title: "添加到喜欢"},
-			page:  func() model.Page { return likeSong(m.netease, true, false) },
-		}, {
-			title: model.MenuItem{Title: "从喜欢移除"},
-			page:  func() model.Page { return likeSong(m.netease, false, false) },
-		}, {
-			title: model.MenuItem{Title: "添加至歌单"},
-			page:  func() model.Page { return openAddSongToUserPlaylistMenu(m.netease, false, true) },
-		}, {
-			title: model.MenuItem{Title: "从歌单移除"},
-			page:  func() model.Page { return openAddSongToUserPlaylistMenu(m.netease, false, false) },
-		}, {
-			title:  model.MenuItem{Title: "在网页打开"},
-			action: func() { openInWeb(m.netease, false) },
-		}, {
-			title: model.MenuItem{Title: "标记为不喜欢"},
-			page:  func() model.Page { return trashSong(m.netease, false) },
-		}, {
-			title:  model.MenuItem{Title: "相似的歌曲"},
-			action: func() { findSimilarSongs(m.netease, false) },
-		}, {
+	if m.playing || canShare(menu) {
+		actions = append(actions, ActionItem{
 			title:  model.MenuItem{Title: "分享"},
-			action: func() { shareItem(m.netease, false) },
-		},
+			action: func() { shareItem(m.netease, isSelected) },
+		})
 	}
+
+	if m.playing || canOpenInWeb(menu) {
+		actions = append(actions, ActionItem{
+			title:  model.MenuItem{Title: "在网页打开"},
+			action: func() { openInWeb(m.netease, isSelected) },
+		})
+	}
+
+	m.items = actions
+	return nil
 }
 
-func (m *ActionMenu) playlistAction() {
-	m.item = []ActionItem{
+func buildPlaylistActions(n *Netease) []ActionItem {
+	items := []ActionItem{
 		{
 			title: model.MenuItem{Title: "收藏"},
-			page:  func() model.Page { return collectSelectedPlaylist(m.netease, true) },
+			page:  func() model.Page { return collectSelectedPlaylist(n, true) },
 		}, {
 			title: model.MenuItem{Title: "取消收藏"},
-			page:  func() model.Page { return collectSelectedPlaylist(m.netease, false) },
+			page:  func() model.Page { return collectSelectedPlaylist(n, false) },
 		},
 	}
+	return items
+}
+
+func buildSongActions(n *Netease, isSelected bool) []ActionItem {
+	items := []ActionItem{
+		{
+			title:  model.MenuItem{Title: "所属专辑"},
+			action: func() { goToAlbumOfSong(n, isSelected) },
+		},
+		{
+			title:  model.MenuItem{Title: "所属歌手"},
+			action: func() { goToArtistOfSong(n, isSelected) },
+		},
+		{
+			title:  model.MenuItem{Title: "下载"},
+			action: func() { downloadSong(n, isSelected) },
+		},
+		{
+			title:  model.MenuItem{Title: "下载歌词"},
+			action: func() { downloadSongLrc(n, isSelected) },
+		},
+		{
+			title: model.MenuItem{Title: "添加到喜欢"},
+			page:  func() model.Page { return likeSong(n, true, isSelected) },
+		},
+		{
+			title: model.MenuItem{Title: "从喜欢移除"},
+			page:  func() model.Page { return likeSong(n, false, isSelected) },
+		},
+		{
+			title: model.MenuItem{Title: "添加至歌单"},
+			page:  func() model.Page { return openAddSongToUserPlaylistMenu(n, isSelected, true) },
+		},
+		{
+			title: model.MenuItem{Title: "从歌单移除"},
+			page:  func() model.Page { return openAddSongToUserPlaylistMenu(n, isSelected, false) },
+		},
+		{
+			title: model.MenuItem{Title: "标记为不喜欢"},
+			page:  func() model.Page { return trashSong(n, isSelected) },
+		},
+		{
+			title:  model.MenuItem{Title: "相似的歌曲"},
+			action: func() { findSimilarSongs(n, isSelected) },
+		},
+	}
+	return items
+}
+
+func canShare(menu model.Menu) bool {
+	if _, ok := menu.(composer.Sharer); ok {
+		return true
+	}
+	switch menu.(type) {
+	case SongsMenu, AlbumsMenu, ArtistsMenu, PlaylistsMenu:
+		return true
+	default:
+		return false
+	}
+}
+
+func canOpenInWeb(menu model.Menu) bool {
+	// 判断逻辑一致
+	return canShare(menu)
+}
+
+func canCollectPlaylist(menu model.Menu) bool {
+	_, ok := menu.(PlaylistsMenu)
+	return ok
+}
+
+func isSongsProvider(menu model.Menu) bool {
+	_, ok := menu.(SongsMenu)
+	return ok
 }
